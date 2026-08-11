1a1265c1a234a8791f1cb003e1e9e9d63796f2b5
hodienhung <lynguyen20013@gmail.com>
2026-08-11 17:24:10 +0700

    Create TicketRequest.java

---
+++ TicketRequest.java
@@ -0,0 +1,13 @@
+package com.example.parking.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketRequest {
+    private Long vehicleId;
+    private Long zoneId;
+}
